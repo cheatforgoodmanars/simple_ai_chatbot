f0439549d47fe5a39f325d8e17808104681ea72e
Testforgood <117313179+cheatforgoodmanars@users.noreply.github.com>
2025-02-20 17:47:08 +0400

    Simple functional chatbot

--- chatbot.py
+++ chatbot.py
@@ -8,10 +8,32 @@ model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 conversation_history = []
-# Encoding the conversation history
-history_string = "\n".join(conversation_history)
-# Fetch prompt from user
-input_text ="hello, how are you doing?"
-# Tokenization of user prompt and chat history
-inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
-print(inputs)
+while True:
+    # Encoding the conversation history \Create conversation history string
+    history_string = "\n".join(conversation_history)
+
+    # # Fetch prompt from user
+    # input_text ="hello, how are you doing?"
+    # Tokenization of user prompt and chat history
+
+    # Get the input data from the user
+    input_text = input("> ")
+    # Tokenize the input text and history
+    inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
+    print(inputs)
+
+    tokenizer.pretrained_vocab_files_map
+
+    # Generate output from the model
+    outputs = model.generate(**inputs)
+    print(outputs)
+
+    # decode output
+    response= tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+    print(response)
+
+    # Update conversation history
+    conversation_history.append(input_text)
+    conversation_history.append(response)
+    print(conversation_history)
+
