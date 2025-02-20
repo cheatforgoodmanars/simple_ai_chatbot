391119f5441d1e03a6b9d1d4006e2150c401de38
Testforgood <117313179+cheatforgoodmanars@users.noreply.github.com>
2025-02-20 16:56:33 +0400

    Conversation detals 1

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,17 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+
+model_name = "facebook/blenderbot-400M-distill"
+
+# Load model (download on first run and reference local installation for consequent runs)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+conversation_history = []
+# Encoding the conversation history
+history_string = "\n".join(conversation_history)
+# Fetch prompt from user
+input_text ="hello, how are you doing?"
+# Tokenization of user prompt and chat history
+inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
+print(inputs)
